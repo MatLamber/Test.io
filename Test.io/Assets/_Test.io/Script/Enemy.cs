6e277eabd6e9941359e093f4553b4c0e0d1d4cf0
MatLamber <matixlambaer@gmail.com>
2024-11-20 17:18:08 -0300

    Pre animator cambio

--- Test.io/Assets/_Test.io/Script/Enemy.cs
+++ Test.io/Assets/_Test.io/Script/Enemy.cs
@@ -13,19 +13,21 @@ public struct EnemyDeathEvent
 {
     public static EnemyDeathEvent e;
     public Enemy Enemy;
+
     public static void Trigger(Enemy enemyData)
     {
         e.Enemy = enemyData;
         MMEventManager.TriggerEvent(e);
     }
 }
+
 public class Enemy : MonoBehaviour
 {
     private Transform target;
 
     // Referencia al componente AIPath
     private AIPath aiPath => GetComponent<AIPath>();
-    
+
     private AgentAuthoring agentAuthoring => GetComponent<AgentAuthoring>();
 
     // Referencia al componente Animation
@@ -37,12 +39,13 @@ public class Enemy : MonoBehaviour
     [SerializeField] private string attackAnimation = "Attack";
     [SerializeField] private string flinchAnimation = "Flinch";
     [SerializeField] private string deathAnimation = "Flinch";
-    [SerializeField] private string fallingPoseAnimation = "FallingPose";   
-    [SerializeField] private string lyingPoseAnimation = "LyingPose";   
+    [SerializeField] private string fallingPoseAnimation = "FallingPose";
+    [SerializeField] private string lyingPoseAnimation = "LyingPose";
+    [SerializeField] private string deafultDeathAnimation = "LyingPose";
 
     [SerializeField] private Transform enemyModel;
     [SerializeField] private Transform landingPosition;
- 
+
 
     private bool isDead;
 
@@ -58,30 +61,27 @@ public class Enemy : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            PlayDeathAnimation();
+            PlayFlyAwayDeath();
         }
     }
 
     private void OnDisable()
     {
-     //   GetComponent<TopDownController3D>().enabled = true;
         GetComponent<CharacterController>().enabled = true;
-       GetComponent<Rigidbody>().isKinematic = false;
-       enemyModel.localPosition = Vector3.zero;
-       enemyModel.localRotation = Quaternion.Euler(Vector3.zero);
+        GetComponent<Rigidbody>().isKinematic = false;
+        enemyModel.localPosition = Vector3.zero;
+        enemyModel.localRotation = Quaternion.Euler(Vector3.zero);
         isDead = false;
-        if(aiPath != null)
+        if (aiPath != null)
             aiPath.canMove = true;
     }
 
-    public  void FollowTarget()
+    public void FollowTarget()
     {
-        if(isDead) return;
+        if (isDead) return;
 
         if (aiPath != null && target != null)
         {
-        
-      
             aiPath.destination = target.transform.position;
 
             // Ejemplo: Cambiar animación a 'Run' si se mueve
@@ -89,7 +89,6 @@ public class Enemy : MonoBehaviour
             {
                 if (anim.IsPlaying(idleAnimation))
                 {
-                   
                     InstantTransitionTo(runAnimation);
                 }
             }
@@ -102,54 +101,43 @@ public class Enemy : MonoBehaviour
             }
         }
     }
-    
+
     public void FollowAgentAuthoring()
     {
         if (agentAuthoring != null && target != null)
         {
-            /*var body = agentAuthoring.EntityBody;
-            body.Destination = target.position;
-            body.IsStopped = false;
-            agentAuthoring.EntityBody = body;*/
             agentAuthoring.SetDestination(target.position);
         }
     }
 
     public void PlayFlinchAnimation()
     {
-        if(aiPath != null)
+        if (aiPath != null)
             aiPath.canMove = false;
         InstantTransitionTo(flinchAnimation, returnTo: idleAnimation);
         StartCoroutine(ResetCanMove(flinchAnimation));
     }
 
-    public void PlayDeathAnimation()
+    public void PlayFlyAwayDeath()
     {
         if (aiPath != null) aiPath.canMove = false;
         GetComponent<Rigidbody>().isKinematic = true;
         anim.Stop();
 
         // Eleva al enemigo y añade rotación
-        int jumpHeight = Random.Range(6,23) ;
-        float jumpDuration = Random.Range(0.6f, 1.8f) ;
+        int jumpHeight = Random.Range(6, 23);
+        float jumpDuration = Random.Range(0.6f, 1.8f);
         float fallDuration = jumpDuration / 2;
-        float rotationDuration = jumpDuration + fallDuration/2; // Duración total incluyendo la bajada
+        float rotationDuration = jumpDuration + fallDuration / 2; // Duración total incluyendo la bajada
         float delayBeforeDescent = Random.Range(1f, 1.3f); // Tiempo que se espera antes de descender;
 
         // Animación de elevarse
         enemyModel.DOMoveY(enemyModel.localPosition.y + jumpHeight, jumpDuration)
-            .OnStart((() => anim.Play(fallingPoseAnimation)))
-            .OnComplete(() =>
-            {
-                // Reproduce la animación de muerte después de la elevación
-                // anim.Play(deathAnimation);
-            });
+            .OnStart((() => anim.Play(fallingPoseAnimation)));
 
         // Animación de descenso con retraso
-        enemyModel.DOMoveY(enemyModel.localPosition.y, fallDuration).SetDelay(delayBeforeDescent).SetEase(ease: Ease.InQuad).OnComplete((() =>
-        {
-            anim.Play(lyingPoseAnimation);
-        }));
+        enemyModel.DOMoveY(enemyModel.localPosition.y, fallDuration).SetDelay(delayBeforeDescent)
+            .SetEase(ease: Ease.InQuad).OnComplete((() => { anim.Play(lyingPoseAnimation); }));
 
         int rotatioDirection = Random.Range(0, 2);
 
@@ -158,33 +146,32 @@ public class Enemy : MonoBehaviour
         {
             case 0:
                 // Animación de rotación continua durante la elevación y el descenso
-                enemyModel.DORotate(new Vector3(1080,rotationY , enemyModel.localRotation.z), rotationDuration, RotateMode.FastBeyond360)
+                enemyModel.DORotate(new Vector3(1080, rotationY, enemyModel.localRotation.z), rotationDuration,
+                        RotateMode.FastBeyond360)
                     .SetEase(Ease.OutQuad).OnComplete((() => anim.Play(lyingPoseAnimation)));
                 break;
             case 1:
                 // Animación de rotación continua durante la elevación y el descenso
-                enemyModel.DORotate(new Vector3(1080,rotationY , enemyModel.localRotation.z), rotationDuration, RotateMode.FastBeyond360)
+                enemyModel.DORotate(new Vector3(1080, rotationY, enemyModel.localRotation.z), rotationDuration,
+                        RotateMode.FastBeyond360)
                     .SetEase(Ease.OutQuad).OnComplete((() => anim.Play(lyingPoseAnimation)));
                 break;
         }
-        /*float rotationY = transform.rotation.eulerAngles.y;
-        switch (rotatioDirection)
-        {
-            case 0:
-                // Animación de rotación continua durante la elevación y el descenso
-                transform.DORotate(new Vector3(transform.rotation.x + 1800, rotationY, transform.rotation.z), rotationDuration,
-                        RotateMode.FastBeyond360)
-                    .SetEase(Ease.InOutQuad);
-                break;
-            case 1:
-                // Animación de rotación continua durante la elevación y el descenso
-                transform.DORotate(new Vector3(transform.rotation.x, rotationY, transform.rotation.z + 1800), rotationDuration,
-                        RotateMode.FastBeyond360)
-                    .SetEase(Ease.InOutQuad);
-                break;
-        }*/
 
+        isDead = true;
+        EnemyDeathEvent.Trigger(this);
+    }
 
+    public void PlayDeafultDeathAnimation()
+    {
+        if (aiPath != null) aiPath.canMove = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        anim.Stop();
+        
+        
+        
+        anim.Play(deafultDeathAnimation);
+        enemyModel.DOJump(landingPosition.position, 1, 1,0.5f);
         isDead = true;
         EnemyDeathEvent.Trigger(this);
     }
@@ -192,7 +179,7 @@ public class Enemy : MonoBehaviour
     IEnumerator ResetCanMove(string animationName)
     {
         yield return new WaitForSeconds(GetAnimationClipLength(animationName));
-        if(!isDead && aiPath != null)
+        if (!isDead && aiPath != null)
             aiPath.canMove = true;
     }
 
@@ -226,10 +213,8 @@ public class Enemy : MonoBehaviour
             // Cambiar de vuelta a la animación original
             anim.CrossFade(returnAnimation);
         }
-     
-  
     }
-    
+
     private float GetAnimationClipLength(string clipName)
     {
         AnimationClip clip = anim.GetClip(clipName);
